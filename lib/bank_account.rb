32c7b8039135826983af7d43829d871991682139
Sushi Chanrai <sushi@pathworks.co>
2018-07-19 22:28:41 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -27,15 +27,6 @@ end
 
 =begin
 
-describe '#valid?' do
-  it "is valid with an open status and a balance greater than 0" do
-    @broke = BankAccount.new("Kat Dennings")
-    @broke.balance = 0
-    @closed = BankAccount.new("Beth Behrs")
-    @closed.status = "closed"
-    expect(avi.valid?).to eq(true)
-    expect(@broke.valid?).to eq(false)
-    expect(@closed.valid?).to eq(false)
-  end
-end
+
+
 =end
