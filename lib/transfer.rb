373266cdf78067328bcfe837c6130d5f479000a5
Sushi Chanrai <sushi@pathworks.co>
2018-07-19 22:32:59 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,9 @@
 class Transfer
   # your code here
+  attr_reader
+  attr_accessor :name, :sender, :receiver
 
-
-    def initialize(name)
+    def initialize(name, sender, receiver)
     end
 
 end
