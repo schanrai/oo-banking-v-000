0ae3529c20cc7bf375969fab83c64d70153457be
Sushi Chanrai <sushi@pathworks.co>
2018-07-19 22:15:13 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,6 +9,15 @@ class BankAccount
     @status = "open"
   end
 
+  def deposit
+  end
+
+  describe '#deposit' do
+    it "can deposit money into its account" do
+      expect(avi.balance).to eq(1000)
+      avi.deposit(1000)
+      expect(avi.balance).to eq(2000)
+    end
 
 
 
