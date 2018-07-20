9107ccc0af3852dd0134ca9f196b0a220bb8574e
Sushi Chanrai <sushi@pathworks.co>
2018-07-20 14:14:52 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
     end
 
     def execute_transaction
-      binding.pry
+      #binding.pry
       if sender.status == "complete"
         return
       elsif self.valid? == false
