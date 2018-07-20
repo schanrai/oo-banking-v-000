deced11b15211bac9287fd993419b2bdf124107c
Sushi Chanrai <sushi@pathworks.co>
2018-07-20 16:54:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,9 +18,7 @@ class Transfer
 
     def execute_transaction
       #binding.pry
-      if self.status == "complete"
-        return
-      elsif self.status == "pending" && sender.balance > @amount && valid?
+      if self.status == "pending" && sender.balance > @amount && sender.valid?
       sender.balance = sender.balance - @amount
       receiver.balance = receiver.balance + @amount
       @status = "complete"
