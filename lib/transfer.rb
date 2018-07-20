07a410e327223147c2536c70f0874674b3338157
Sushi Chanrai <sushi@pathworks.co>
2018-07-20 14:42:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,8 @@ class Transfer
       if self.status == "complete"
         return
       elsif sender.valid? == false
-        "Transaction rejected. Please check your account balance."
         self.status = "rejected"
+        "Transaction rejected. Please check your account balance."
       else
       sender.balance = sender.balance - @amount
       receiver.balance = receiver.balance + @amount
